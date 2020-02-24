99fc806bbb8d392c9ac6f9f2d5776a935d6a20cb
aloksingh3112 <aloksingh3112@gmail.com>
2020-02-24 22:55:20 +0530

    added  header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HamburgerComponent } from './header/hamburger/hamburger.component';
+import { NavProfileComponent } from './header/nav-profile/nav-profile.component';
+import { SearchComponent } from './header/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    SidebarComponent
+    SidebarComponent,
+    HamburgerComponent,
+    NavProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
