e0a411664be2efb2b7f69c80196b4ac4172d5456
aloksingh3112 <aloksingh3112@gmail.com>
2020-02-24 22:20:47 +0530

    created header,main,sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
